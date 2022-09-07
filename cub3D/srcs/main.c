082938547220a3bb6e9ff22599a09a5dc67a68c9
kim-mg <audrjs3535@naver.com>
2022-09-07 21:07:10 +0900

    cub3d update

--- cub3D/srcs/main.c
+++ cub3D/srcs/main.c
@@ -11,7 +11,7 @@
 #define X_EVENT_DESTROY_NOTIFY	17
 
 #define TILE_SIZE	64
-#define MINIMAP_SIZE	20
+#define MINIMAP_SIZE	64
 
 # define TO_COORD(X, Y, W) ((int)floor(Y) * W + (int)floor(X))
 
@@ -964,7 +964,7 @@ void	draw_3d(t_game *game)
 		// 맵에서의 위치
 		int map_x = (int)game->player.pos_x;
 		int map_y = (int)game->player.pos_y;
-
+		
 		// 현재 위치에서 다음 x측 또는 y측까지 ray의 거리
 		double	sideDist_x;
 		double	sideDist_y;
@@ -1061,30 +1061,148 @@ void	draw_3d(t_game *game)
 	}
 }
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define  EPS            (1e-06)
+#define  is_zero(d)     (fabs(d) < EPS)
+#define  deg2rad(d)     ((d)*M_PI/180.0)    /* degree to radian */
+#define  rad2deg(d)     ((d)*180.0/M_PI)    /* radian to degree */
+#define  min(a,b)       ((a)<(b)? (a):(b))
+#define  max(a,b)       ((a)>(b)? (a):(b))
+
+#define  SX         2112       /* screen width */
+#define  FOV        10      /* field of view (in degree) */
+#define  FOV_H      deg2rad(FOV)
+
+static const double ANGLE_PER_PIXEL = FOV_H / (SX-1.);
+static const double FOVH_2 = FOV_H / 2.0;
+
+enum { VERT, HORIZ };
+
+typedef enum { false=0, true=1 } bool;
+typedef enum { DIR_N=0, DIR_E, DIR_W, DIR_S } dir_t;
+
+char	map_get_cell(t_game *game, int x, int y )
+{
+    return (x >= 0 && x < game->map.rows && y >= 0 && y < game->map.cols) ? game->map.coord[x][y] : '\0';
+}
+
+int	sgn( double d )
+{
+    return is_zero(d) ? 0 : ((d > 0) ? 1 : -1);
+}
+
+double	l2dist( double x0, double y0, double x1, double y1 )
+{
+    double dx = x0 - x1;
+    double dy = y0 - y1;
+    return sqrt(dx*dx + dy*dy);
+}
+
+bool	get_wall_intersection( double ray, double px, double py, dir_t* wdir, double* wx, double* wy, t_game *game )
+{
+    int xstep = sgn( cos(ray) );  /* +1 (right), 0 (no change), -1 (left) */
+    int ystep = sgn( sin(ray) );  /* +1 (up),    0 (no change), -1 (down) */
+
+    double xslope = (xstep == 0) ? INFINITY : tan(ray);
+    double yslope = (ystep == 0) ? INFINITY : 1./tan(ray);
+
+    double nx = (xstep > 0) ? floor(px)+1 : ((xstep < 0) ? ceil(px)-1 : px);
+    double ny = (ystep > 0) ? floor(py)+1 : ((ystep < 0) ? ceil(py)-1 : py);
+
+    printf("\nray=%.2f deg, xstep=%d, ystep=%d, xslope=%.2f, yslope=%.2f, nx=%.2f, ny=%.2f\n",
+		rad2deg(ray), xstep, ystep, xslope, yslope, nx, ny);
+
+    double f=INFINITY, g=INFINITY;
+    bool hit = false;
+    int hit_side; /* either VERT or HORIZ */
+
+    while( !hit )
+    {
+        int mapx, mapy;
+
+        if( xstep != 0 ) f = xslope * (nx-px) + py;
+        if( ystep != 0 ) g = yslope * (ny-py) + px;
+
+        /* which is nearer to me - VERT(nx,f) or HORIZ(g,ny)? */
+        double dist_v = l2dist(px, py, nx, f);
+        double dist_h = l2dist(px, py, g, ny);
+
+        if( dist_v < dist_h ) { /* VERT is nearer; go along x-axis */
+            mapx = (xstep == 1) ? (int)(nx) : (int)(nx)-1 ;
+            mapy = (int) f;
+            hit_side = VERT;
+            printf(" V(%d, %.2f) ->", mapx, f);
+        }
+        else {  /* HORIZ is nearer; go along y-axis */
+            mapx = (int) g;
+            mapy = (ystep == 1) ? (int)(ny) : (int)(ny)-1 ;
+            hit_side = HORIZ;
+            printf(" H(%.2f, %d) ->", g, mapy);
+        }
+        char cell = map_get_cell(game, mapy, mapx);
+        if( cell == '\0' ) break;   /* out of map */
+
+        if( cell == '1' ) {   /* hit wall? */
+            if( hit_side == VERT ) {
+                *wdir = (xstep > 0) ? DIR_W : DIR_E;
+                *wx = nx;
+                *wy = f;
+            }
+            else { /* HORIZ */
+                *wdir = (ystep > 0) ? DIR_S : DIR_N;
+                *wx = g;
+                *wy = ny;
+            }
+            hit = true;
+            printf(" hit wall!\n");
+            break;
+        }
+
+        if( hit_side == VERT ) nx += xstep;
+        else ny += ystep;
+    }
+    /* end of while(!hit) */
+
+    return hit;
+}
+
+double	cast_single_ray( int x, double px, double py, double th, t_game *game )
+{
+    double ray = (th + FOVH_2) - (x * ANGLE_PER_PIXEL);
+
+    dir_t wdir;     /* direction of wall */
+    double wx, wy;  /* coord. of wall intersection point */
+
+    if( get_wall_intersection(ray, px, py, &wdir, &wx, &wy, game) == false )
+        return INFINITY; /* no intersection - maybe bad map? */
+
+    double wdist = l2dist(px, py, wx, wy);
+
+    return wdist;
+}
+
 void	draw_rays(t_game *game)
 {
-	double	fov_v;
-	double	fov_h;
-
-	fov_h = 1.0 / 6.0;
-	fov_v = game->map.height / game->map.width * fov_h;
-
-	int	i;
-	int	j;
-	double	y;
-	double	dir;
-
-	dir = 1;
-	
-	while (dir < 2)
+	for (int x = 0; x < game->map.width; ++x)
 	{
-		i = -1;
-		while (++i < game->map.width)
+		// ray 위치와 방향 계산
+		double	camera_x = 2 * x / (double)game->map.width - 1;
+		double	rayDir_x = game->player.dir_x + game->player.plane_x * camera_x;
+		double	rayDir_y = game->player.dir_y + game->player.plane_y * camera_x;
+		// printf("rayDir_x: %f\trayDir_y: %f\n", rayDir_x, rayDir_y);
+		if ((int)(rayDir_y * game->player.pos_x * TILE_SIZE) == (int)(game->player.pos_y * TILE_SIZE))
 		{
-			y = dir * i;
-			game->img.data[(int)y * game->map.width + i] = 0xFFFF00;
+			for (int i = 0; i < game->map.width; ++i)
+			{
+				int	y = (int)fabs(rayDir_y * i);
+				// printf("y: %d\n", y);
+				if (y <= (int)(game->player.pos_y * TILE_SIZE))
+					game->img.data[y * game->map.width + i] = 0xFFFF00;
+			}
 		}
-		dir += 0.15;
 	}
 }
 
@@ -1109,12 +1227,12 @@ void	draw_layer(t_game *game)
 
 int	main_loop(t_game *game)
 {
-	draw_layer(game);
+	// draw_layer(game);
 	draw_3d(game);
 	draw_rectangles(game);
 	draw_lines(game);
 	draw_player(game);
-	// draw_rays(game);
+	draw_rays(game);
 	struct timeval	tv;
 
 	gettimeofday(&tv, NULL);
@@ -1155,6 +1273,7 @@ int	main(int argc, char *argv[])
 	// execute_game(&game);
 	test_img_init(&game);
 	mlx_loop_hook(game.mlx, &main_loop, &game);
+	draw_rays(&game);
 	mlx_loop(game.mlx);
 	free_game(&game);
 	return (0);
